9c4a293090645997b0191a9dea2ccde513c0bbf3
Diones <pereiradiones987@gmail.com>
2025-03-18 12:08:26 -0300

    service de autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  googleLogin(req) {
+    if (!req.user) {
+      return 'Nenhum usuário do Google'
+    }
+
+    return {
+      message: 'Informações do usuário do Google',
+      user: req.user
+    }
+  }
+}
