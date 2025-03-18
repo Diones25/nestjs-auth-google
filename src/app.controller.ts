4f12c9532f8752e98fe7e6ac756157cd3d7ae84f
Diones <pereiradiones987@gmail.com>
2025-03-18 15:09:37 -0300

    rota para a tela home

---
+++ src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Res } from "@nestjs/common";
+import { Response } from "express";
+
+@Controller()
+export class AppController{
+  
+  @Get()
+  getHome(@Res() res: Response) {
+    res.render('index'); // Renderiza a página Home
+  }
+}
