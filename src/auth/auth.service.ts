268900fbe49011e97f4017d9a858746aa212712e
Diones <pereiradiones987@gmail.com>
2025-03-18 16:21:11 -0300

    método para gerar token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) { }
+  
+  async generateJwtToken(user: any): Promise<string> {
+    const payload = { email: user.email, sub: user.id };
+    return this.jwtService.sign(payload);
+  }
 
   googleLogin(req) {
     if (!req.user) {
