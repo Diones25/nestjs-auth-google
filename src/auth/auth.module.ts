287a3520623fb015f5e4d1be914f4e4197aca56e
Diones <pereiradiones987@gmail.com>
2025-03-18 12:06:15 -0300

    modulo de autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [PassportModule.register({ defaultStrategy: 'google' })],
+  providers: [GoogleStrategy, AuthService],
   controllers: [AuthController]
 })
 export class AuthModule {}
